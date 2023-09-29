310960500227359af5da6232bef89f60a13f36f6
KosmX <kosmx.mc@gmail.com>
2023-09-29 22:12:35 +0200

    some fixes

--- src/main/kotlin/hu/bme/kszk/inverseCaptcha/Application.kt
+++ src/main/kotlin/hu/bme/kszk/inverseCaptcha/Application.kt
@@ -11,7 +11,7 @@ import io.ktor.server.freemarker.*
 import io.ktor.server.netty.*
 
 fun main() {
-    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+    embeddedServer(Netty, port = 80, host = "0.0.0.0", module = Application::module)
         .start(wait = true)
 }
 
--- src/main/kotlin/hu/bme/kszk/inverseCaptcha/CaptchaHandler.kt
+++ src/main/kotlin/hu/bme/kszk/inverseCaptcha/CaptchaHandler.kt
@@ -9,6 +9,7 @@ import io.ktor.server.response.*
 import io.ktor.util.pipeline.*
 import kotlinx.coroutines.coroutineScope
 import java.time.Instant
+import kotlin.math.abs
 import kotlin.time.toJavaDuration
 
 
@@ -32,13 +33,13 @@ suspend fun PipelineContext<Unit, ApplicationCall>.createAndPostCaptcha(recreate
 
 suspend fun PipelineContext<Unit, ApplicationCall>.verifyAndPost() = coroutineScope {
     val solution = call.receiveParameters()["solution"]?.toDoubleOrNull()
-    if (solution == null) throw BadRequestException("post parameter is not a number")
+        ?: throw BadRequestException("post parameter is not a number")
 
     if (session.dueTime < Instant.now()) {
         return@coroutineScope createAndPostCaptcha(true)
     }
 
-    if (solution == session.current?.result && session.dueTime >= Instant.now()) {
+    if (solution closeTo (session.current?.result) && session.dueTime >= Instant.now()) {
         session = session.copy(solved = session.solved + 1)
 
         if (session.solved >= Util.requiredAmount) {
@@ -46,4 +47,9 @@ suspend fun PipelineContext<Unit, ApplicationCall>.verifyAndPost() = coroutineSc
         }
     }
     createAndPostCaptcha(true)
+}
+
+private infix fun Double?.closeTo(other: Double?): Boolean {
+    if (this == null || other == null) return false
+    return abs(this - other) < this * Util.delta
 }
